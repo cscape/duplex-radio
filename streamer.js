ad2a2d30a65358579c2f46abeb81c632bc7e43a9
Glitch (hello-express) <none>
2019-07-04 03:53:23 +0000

    🏹🐙 Checkpoint ./streamer.js:1228437/84 ./server.js:1228437/20

--- streamer.js
+++ streamer.js
@@ -1,16 +1,17 @@
 const https = require('https')
-const { Readable } = require('stream')
+const { Readable, Duplex } = require('stream')
 
 const DuplexStreamer = ds => https.get('https://vapor.fm:8000/stream', res => {
   ds.readable = true
   ds.setEncoding('binary')
   ds.resume()
   res.setEncoding('binary')
-  res.on('data', chunk => ds.emit('data', chunk))
+  res.on('data', chunk => ds.push(chunk))
 }).on('error', e => console.error(`Got error: ${e.message}`))
 
 const getStream = () => {
-  const MusicStream = new Readable()
+  const MusicStream = new Duplex()
+  MusicStream._read = _ => 0;
   DuplexStreamer(MusicStream)
   return MusicStream
 }
